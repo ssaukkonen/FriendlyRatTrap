ceff91e5bc96637a4db3a087bc2b0cc7e382ec19
Samuli Saukkonen <ssaukkonen@gmail.com>
2021-11-22 12:22:46 +0200

    ekat kooditestaukset kameralle

---
+++ raspiCamera/testCam.py
@@ -0,0 +1,26 @@
+from picamera import PiCamera
+from time import sleep
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def numpyCamera():
+    while True:
+        camera.resolution = (320,240)
+        camera.framerate = 3
+        output = np.empty((240,320, 3), dtype=np.uint8)
+        camera.capture(output, 'rgb')
+        print(output)
+        plt.imshow(output)
+        plt.show()
+    
+
+def captureImage():
+    camera.resolution = (320,240)
+    camera.framerate = 3
+    camera.start_preview()
+    sleep(2)
+    camera.capture('image.jpg')
+    
+camera = PiCamera()
+captureImage()
