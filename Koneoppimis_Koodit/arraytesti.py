43233a2864b257dd197dc307ddcb71753f7356b6
Pauli Rekila <t0repa00@students.oamk.fi>
2021-11-24 14:47:25 +0200

    tflite lisatty seka uusin malli

--- Koneoppimis_Koodit/arraytesti.py
+++ Koneoppimis_Koodit/arraytesti.py
@@ -1,7 +1,4 @@
 import numpy as np
 
-jakaja = np.ones((1,4))
-
-
-print(np.shape(jakaja))
+print(np.__version__)
 #print(jakaja)
--- Koneoppimis_Koodit/model_use.py
+++ Koneoppimis_Koodit/model_use.py
@@ -5,7 +5,7 @@ from PIL import Image
 from numpy import asarray
 
 # Load TFLite model and allocate tensors.
-interpreter = tf.lite.Interpreter(model_path="Model\model.tflite")
+interpreter = tf.lite.Interpreter(model_path="Model\model_1.tflite")
 interpreter.allocate_tensors()
 
 # Get input and output tensors.
@@ -14,14 +14,15 @@ output_details = interpreter.get_output_details()
 
 # Test model on random input data.
 input_shape = input_details[0]['shape']
-#img = cv2.imread('Testikuvat\image.jpg')
-#img = cv2.resize(img,(320,240))
-img = Image.open('Testikuvat\image.jpg')
-numpydata = asarray(img, dtype=np.float32)
-numpydata2 = np.expand_dims(numpydata, axis=0)
-print(np.shape(numpydata))
+img = cv2.imread('Testikuvat\image.jpg')
+img = cv2.resize(img,(224,224))
+img = np.expand_dims(img, axis=0)
+#img = Image.open('Testikuvat\image.jpg')
+#numpydata = asarray(img, dtype=np.float32)
+#numpydata2 = np.expand_dims(numpydata, axis=0)
+#print(np.shape(numpydata))
 
-input_data = numpydata2
+input_data = img
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
 interpreter.invoke()
