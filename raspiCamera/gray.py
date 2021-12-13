62b0f46b411eb2e5c075de78db3211953093762d
Samuli Saukkonen <ssaukkonen@gmail.com>
2021-12-13 12:06:46 +0200

    Siivottu turhasta roinasta

--- raspiCamera/gray.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-
-img = cv2.imread('image.jpg',cv2.IMREAD_COLOR)
-grayscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-result=cv2.imwrite('gray_image.jpg', grayscale)
