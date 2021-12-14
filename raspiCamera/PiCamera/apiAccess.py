7160455660cad6af11beda2387bc9e96dc79d8a9
Samuli Saukkonen <ssaukkonen@gmail.com>
2021-12-14 13:33:30 +0200

    Lisätty printtejä demoa varten

--- raspiCamera/PiCamera/apiAccess.py
+++ raspiCamera/PiCamera/apiAccess.py
@@ -9,21 +9,27 @@ class APIAccess:
         self.url = 'http://' + self.credentials[0] + ':' + self.credentials[1] + '@172.20.241.184/'
 
     def send_data_to_server(self, result):
-        print('sending')
+        print('Sending picture to server')
         try:
             requests.post(self.url + 'img/', files={'image': result}, timeout=1)
             return True
         except Exception as e:
             print(e)
+            time.sleep(10)
             return False
         
     def get_trapActive_status(self):
-        result = requests.get(self.url + 'wait/')
-        result = json.loads(result.content)
-        result = result['state'][0][0]
-        print(result)
-        if result == 1:
-            return True
-        else:
+        try:
+            result = requests.get(self.url + 'wait/')
+            result = json.loads(result.content)
+            result = result['state'][0][0]
+            if result == 1:
+                print('Reseting trap')
+                return True
+            else:
+                time.sleep(10)
+                return False
+        except Exception as e:
+            print(e)
             time.sleep(10)
             return False
--- raspiCamera/PiCamera/classifyImage.py
+++ raspiCamera/PiCamera/classifyImage.py
@@ -3,8 +3,6 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
-
-#from PIL import Image
 from tflite_runtime.interpreter import Interpreter
 
 class ClassifyImage:
